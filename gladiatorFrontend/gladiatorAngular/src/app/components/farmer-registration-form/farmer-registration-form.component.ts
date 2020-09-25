63f6420c0de752b7c0369643941c1a36c2ec672f
Nikhil <Nikhil.Maroju@lntinfotech.com>
2020-09-25 12:45:35 +0530

    FarmerRegistrationForm Component

---
+++ gladiatorFrontend/gladiatorAngular/src/app/components/farmer-registration-form/farmer-registration-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-farmer-registration-form',
+  templateUrl: './farmer-registration-form.component.html',
+  styleUrls: ['./farmer-registration-form.component.css']
+})
+export class FarmerRegistrationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
