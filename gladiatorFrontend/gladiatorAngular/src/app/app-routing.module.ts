88ed86a06b81f874a42ec7d6b72a79402de665a3
Amlan Abhijeet <35831886+Amlan7@users.noreply.github.com>
2020-09-25 23:21:59 +0530

    formatting

--- gladiatorFrontend/gladiatorAngular/src/app/app-routing.module.ts
+++ gladiatorFrontend/gladiatorAngular/src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { AuctionRequestComponent } from './components/auction-request/auction-request.component';
 import { BidderRegistrationComponent } from './components/bidder-registration/bidder-registration.component';
 import { FarmerRegistrationComponent } from './components/farmer-registration/farmer-registration.component';
+import { FarmerWelcomeComponent } from './components/farmer-welcome/farmer-welcome.component';
 import { HomeComponent } from './components/home/home.component';
 import { SubRegisterPageComponent } from './components/sub-register-page/sub-register-page.component';
 import { ContactusComponent } from './contactus/contactus.component';
 
 const routes: Routes = [
   {path:'subregister', component:SubRegisterPageComponent},
+  {path:'aboutus', component:AboutusComponent},
+  {path:'home', component:HomeComponent},
+  {path:'contactus', component:ContactusComponent},
+  {path:'farmer-welcome', component:FarmerWelcomeComponent},
+  {path:'auction-request', component:AuctionRequestComponent},
 {path:'farmerregister', component:FarmerRegistrationComponent},
 {path:'bidderregister', component:BidderRegistrationComponent}
 ];
--- gladiatorFrontend/gladiatorAngular/src/app/components/auction-request/auction-request.component.ts
+++ gladiatorFrontend/gladiatorAngular/src/app/components/auction-request/auction-request.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { auctionRequest } from 'src/app/Model/auctionRequest';
 import { CroptableService } from 'src/app/services/croptable.service';
 
@@ -10,27 +10,29 @@ import { CroptableService } from 'src/app/services/croptable.service';
   styleUrls: ['./auction-request.component.css']
 })
 export class AuctionRequestComponent implements OnInit {
+  request:auctionRequest=new auctionRequest();
   requestForm: FormGroup;
-  constructor(private formBuilder: FormBuilder,private router: Router,private cropservice:CroptableService) {
+  constructor(private formBuilder: FormBuilder,private router: ActivatedRoute) {
     this.requestForm = this.formBuilder.group({
-			cropType: new FormControl(),
-      cropName: new FormControl(),
-      fertType: new FormControl(),
-      quantity: new FormControl(),
-      basePrice: new FormControl()
+      'cropType': [this.request.cropType, [Validators.required]],
+			'cropName': [this.request.cropName, [Validators.required]],
+      'fertType': [this.request.fertType, [Validators.required]],
+      'quantity': [this.request.quantity, [Validators.required]],
+      'basePrice': [this.request.basePrice, [Validators.required]]
+      
       });
    }
 
   ngOnInit(): void {
   }
-  onSubmit(requestForm:auctionRequest){
-    console.log(requestForm);
+  onSubmit(){
+  //   console.log(requestForm);requestForm:auctionRequest,,private cropservice:CroptableService
 
-  this.cropservice.addAuctionRequest(requestForm).subscribe(data=>{
-    console.log(data)
-    alert("auction request placed successfully");
-    this.router.navigate(['contactus']);
-  })
+  // this.cropservice.addAuctionRequest(requestForm).subscribe(data=>{
+  //   console.log(data)
+  //   alert("auction request placed successfully");
+    //this.router.navigate(['contactus']);
+  //})
 }
 
 }
