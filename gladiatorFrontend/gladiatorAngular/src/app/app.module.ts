4f50b7aff6352872eaffbb09ec93d6fa6eada267
hhhkr-3 <hrishi4321@outlook.com>
2020-09-25 13:14:04 +0530

    Added Aboutus , Contactus components
    Added forms module to app.module.ts
    Created Aboutus- Just basic info about the project, need to work on the content
    
    Created Contactus component
    -Created form in contact us Component

--- gladiatorFrontend/gladiatorAngular/src/app/app.module.ts
+++ gladiatorFrontend/gladiatorAngular/src/app/app.module.ts
@@ -1,34 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
+
+
 import { FarmerRegistrationFormComponent } from './components/farmer-registration-form/farmer-registration-form.component';
-=======
+
 import { LoginComponent } from './components/login/login.component';
 import { AuctionRequestComponent } from './components/auction-request/auction-request.component';
 import { FarmerWelcomeComponent } from './components/farmer-welcome/farmer-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
->>>>>>> 5adb174d063a607815d91ba6fb69319f9505e7b2
+
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-<<<<<<< HEAD
-    FarmerRegistrationFormComponent
-=======
+
+
+    FarmerRegistrationFormComponent,
+
     LoginComponent,
     AuctionRequestComponent,
-    FarmerWelcomeComponent
->>>>>>> 5adb174d063a607815d91ba6fb69319f9505e7b2
+    FarmerWelcomeComponent,
+
+    AboutusComponent,
+    ContactusComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+
+    BrowserAnimationsModule,
+
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
