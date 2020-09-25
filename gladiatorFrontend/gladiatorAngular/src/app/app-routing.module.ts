abab543299e24b386ff629f48c0a02e6f8f1ed52
Nikhil <Nikhil.Maroju@lntinfotech.com>
2020-09-25 22:11:57 +0530

    LoginPage,and some routing paths given

--- gladiatorFrontend/gladiatorAngular/src/app/app-routing.module.ts
+++ gladiatorFrontend/gladiatorAngular/src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { BidderRegistrationComponent } from './components/bidder-registration/bidder-registration.component';
 import { FarmerRegistrationComponent } from './components/farmer-registration/farmer-registration.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { SubRegisterPageComponent } from './components/sub-register-page/sub-register-page.component';
 import { ContactusComponent } from './contactus/contactus.component';
 
 const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'src/app/about',component:AboutusComponent},
+  {path:'src/app/contact',component:ContactusComponent},
   {path:'subregister', component:SubRegisterPageComponent},
 {path:'farmerregister', component:FarmerRegistrationComponent},
-{path:'bidderregister', component:BidderRegistrationComponent}
+{path:'bidderregister', component:BidderRegistrationComponent},
+{path:'login',component:LoginComponent},
+
 ];
 
 @NgModule({
--- gladiatorFrontend/gladiatorAngular/src/app/components/login/login.component.ts
+++ gladiatorFrontend/gladiatorAngular/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { LoginForm } from 'src/app/Models/LoginForm';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: LoginForm =new LoginForm();
+  submitted=false;
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  verifyLogin(){
+
+  }
 }
