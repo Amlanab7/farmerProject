205798f1bb06c61cee8dd4debea9b7c08896916a
Nikhil <Nikhil.Maroju@lntinfotech.com>
2020-09-25 12:57:09 +0530

    commit

--- gladiatorFrontend/gladiatorAngular/src/app/app.module.ts
+++ gladiatorFrontend/gladiatorAngular/src/app/app.module.ts
@@ -4,26 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
-import { FarmerRegistrationFormComponent } from './components/farmer-registration-form/farmer-registration-form.component';
-=======
 import { LoginComponent } from './components/login/login.component';
 import { AuctionRequestComponent } from './components/auction-request/auction-request.component';
 import { FarmerWelcomeComponent } from './components/farmer-welcome/farmer-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
->>>>>>> 5adb174d063a607815d91ba6fb69319f9505e7b2
+import { FarmerRegistrationFormComponent } from './components/farmer-registration-form/farmer-registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-<<<<<<< HEAD
     FarmerRegistrationFormComponent
-=======
-    LoginComponent,
-    AuctionRequestComponent,
-    FarmerWelcomeComponent
->>>>>>> 5adb174d063a607815d91ba6fb69319f9505e7b2
   ],
   imports: [
     BrowserModule,
